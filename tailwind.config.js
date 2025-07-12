074c0895b6036166ea9f799c88d768cb4dd19ec7
suraj-prajapati07 <sp407522@gmail.com>
2025-07-12 15:41:50 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily:{
+        Whitney:["Whitney"],
+        Ginto:["Ginto"],
+        ggSans:["ggSans"],
+      },
+    },
+  },
+  plugins: [],
+  
+}
+
